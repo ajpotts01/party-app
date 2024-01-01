57c402771ae28cf130fed394670b4e48f23d1ec9
Alexander Potts <ajpotts01@gmail.com>
2024-01-01 17:05:22 +1100

    feat: base models/templates (#1)

--- party/admin.py
+++ party/admin.py
@@ -1,3 +1,21 @@
+# Third-party imports
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser, Party, Gift, Guest
+
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    pass
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    readonly_fields: tuple[str] = ("uuid",)
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    readonly_fields: tuple[str] = ("uuid",)
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    readonly_fields: tuple[str] = ("uuid",)
