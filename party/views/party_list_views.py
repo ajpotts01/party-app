cf73d2bc7b2c57575982777f13bb02d60ee44d80
Alexander Potts <ajpotts01@gmail.com>
2024-01-06 00:11:04 +1100

    feature/infinite scroll (#15)
    * feat: infinite scroll
    
    * fix: nad
    
    * fix: formatting

--- party/views/party_list_views.py
+++ party/views/party_list_views.py
@@ -13,8 +13,15 @@ from party.models import Party
 class PartyListPage(LoginRequiredMixin, ListView):
     template_name: str = "party/party_list/page_parties_list.html"
     context_object_name: str = "parties"
+    paginate_by: int = 6  # Even number to match two columns
 
     def get_queryset(self) -> QuerySet:
         return Party.objects.filter(
             organizer=self.request.user, party_date__gte=dt.datetime.today()
         ).order_by("party_date")
+
+    def get_template_names(self) -> list[str]:
+        if "HTTP_HX_REQUEST" in self.request.META:
+            return ["party/party_list/partial_parties_list.html"]
+        else:
+            return ["party/party_list/page_parties_list.html"]
